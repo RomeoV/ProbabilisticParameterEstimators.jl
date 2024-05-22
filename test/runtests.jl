68d29f9ca3450bbf203dbe3df93b09eaf0bceee9
Romeo Valentin <romeov@stanford.edu>
2024-05-22 14:20:41 -0700

    Refactor tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,115 +15,43 @@ using LinearAlgebra
     @test_nowarn mvnoisedistribution(model2)
 end
 
-@testset "MCMCEstimator" begin
-    @testset "predictsamples and predictdist" begin
-        @testset "univariate measurements" begin
-            f(x, p) = (x*p[1] + 1)^2 / p[2]
-            prior = MvNormal(5*ones(2), 3^2 * I)
-            est = MCMCEstimator(prior, f)
-
-            xs = rand(20)
-            θtrue = [1.0, 2.0]
-            noises = [1/10 * rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
-            chain = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 5000)
-            @test mean(chain) ≈ θtrue rtol = 1e-1
-            pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=1000)
-            @test mean(pred) ≈ θtrue rtol = 1e-1
+@testset "predictsamples and predictdist" begin
+    @testset "univariate measurements" begin
+        f(xs, ps) = sum(enumerate(zip(ps, xs))) do (k, (p, x))
+            p*x^k
         end
-        @testset "multivariate measurements" begin
-            f(x, p) = [(x + 1)^2 - sum(p);
-                       (x + 1)^3 + diff(p)[1]]
-            prior = MvNormal(5*ones(2), 3^2 * I)
-            est = MCMCEstimator(prior, f)
 
-            xs = rand(20)
-            θtrue = [1.0, 2.0]
-            noises = [rand() * I(2) * MvNormal(zeros(2), 0.1*I) for _ in eachindex(xs)]
-            # noises = [rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
-            chain = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 1000)
-            @test mean(chain) ≈ θtrue rtol = 1e-1
-            pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=1000)
-            @test mean(pred) ≈ θtrue rtol = 1e-1
-        end
-    end
-end
+        xs = 5 * eachcol(rand(2,8))
+        θtrue = [5., 10]
+        prior = MvNormal(θtrue, 0.5^2 * I)
+        noises = [0.1 * rand() * Normal() for _ in eachindex(xs)]
+        noisemodel = UncorrGaussianNoiseModel(noises)
+        ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
 
-@testset "LSQEstimator" begin
-    @testset "predictsamples and predictdist" begin
-        @testset "univariate measurements" begin
-            f(xs, ps) = sum(enumerate(zip(ps, xs))) do (k, (p, x))
-                p*x^k
-            end
-
-            xs = 5 * eachcol(rand(2,8))
-            θtrue = [5., 10]
-            prior = MvNormal(θtrue, 0.5^2 * I)
-            noises = [0.1 * rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
-
-            est = LSQEstimator(prior, f)
-            samples = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 100)
-            @test mean(samples) ≈ θtrue rtol = 1e-1
-            pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=100)
-            @test mean(pred) ≈ θtrue rtol = 1e-1
-        end
-        @testset "multivariate measurements" begin
-            f(x, p) = [(x + 1)^2 - sum(p);
-                       (x + 1)^3 + diff(p)[1]]
-            prior = MvNormal(zeros(2), 1.0 * I)
-            est = LSQEstimator(prior, f)
-
-            xs = rand(5)
-            θtrue = [1.0, 2.0]
-            noises = [rand()/10 * I(2) * MvNormal(zeros(2), I) for _ in eachindex(xs)]
-            # noises = [rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
+        @testset for est in [MCMCEstimator(prior, f),
+                             LinearApproxEstimator(prior, f),
+                             LSQEstimator(prior, f),]
             samples = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 100)
             @test mean(samples) ≈ θtrue rtol = 1e-1
             pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=100)
             @test mean(pred) ≈ θtrue rtol = 1e-1
         end
     end
-end
+    @testset "multivariate measurements" begin
+        f(x, p) = [(x + 1)^2 - sum(p);
+                    (x + 1)^3 + diff(p)[1]]
 
-@testset "LinearApproxEstimator" begin
-    @testset "predictsamples and predictdist" begin
-        @testset "univariate measurements" begin
-            f(xs, ps) = sum(enumerate(zip(ps, xs))) do (k, (p, x))
-                p*x^k
-            end
+        xs = rand(5)
+        θtrue = [1.0, 2.0]
+        prior = MvNormal(zeros(2), 1.0 * I)
+        noises = [rand()/10 * I(2) * MvNormal(zeros(2), I) for _ in eachindex(xs)]
+        # noises = [rand() * Normal() for _ in eachindex(xs)]
+        noisemodel = UncorrGaussianNoiseModel(noises)
+        ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
 
-            xs = 5 * eachcol(rand(2,8))
-            θtrue = [5., 10]
-            prior = MvNormal(θtrue, 0.5^2 * I)
-            noises = [0.1 * rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
-
-            est = LinearApproxEstimator(prior, f)
-            samples = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 100)
-            @test mean(samples) ≈ θtrue rtol = 1e-1
-            pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=100)
-            @test mean(pred) ≈ θtrue rtol = 1e-1
-        end
-        @testset "multivariate measurements" begin
-            f(x, p) = [(x + 1)^2 - sum(p);
-                       (x + 1)^3 + diff(p)[1]]
-            prior = MvNormal(zeros(2), 1.0 * I)
-            est = LinearApproxEstimator(prior, f)
-
-            xs = rand(5)
-            θtrue = [1.0, 2.0]
-            noises = [rand()/10 * I(2) * MvNormal(zeros(2), I) for _ in eachindex(xs)]
-            # noises = [rand() * Normal() for _ in eachindex(xs)]
-            noisemodel = UncorrGaussianNoiseModel(noises)
-            ysmeas = f.(xs, [θtrue]) .+ rand.(noises)
+        @testset for est in [MCMCEstimator(prior, f),
+                             LinearApproxEstimator(prior, f),
+                             LSQEstimator(prior, f)]
             samples = @test_nowarn predictsamples(est, xs, ysmeas, noisemodel, 100)
             @test mean(samples) ≈ θtrue rtol = 1e-1
             pred = @test_nowarn predictdist(est, xs, ysmeas, noisemodel; nsamples=100)
