1d0819881fe07a365b5bc5962ac46be3c13426aa
Romeo Valentin <romeov@stanford.edu>
2024-05-24 13:55:37 -0700

    Add docstrings for `predictdist` and `predictsamples`

--- src/estimators.jl
+++ src/estimators.jl
@@ -1,8 +1,24 @@
 abstract type EstimationMethod end
 Base.show(io::Base.IO, est::EstimationMethod) = print(io, typeof(est))
 
+"""
+    predictsamples(::EstimationMethod, f, xs, ys, paramprior::Sampleable, noise_model::AbstractNoiseModel, nsamples)
+
+Solve the parameter estimation problem ``y = f(x, θ) + ε`` and return samples of the probability distribution of θ.
+
+Depending on the method, the samples are either generated directly, or sampled from the predicted distribution.
+The `paramprior` is used either as a prior for bayesian methods, or to sample initial guesses for iterative methods.
+"""
 function predictsamples(::EstimationMethod, f, xs, ys, paramprior::Sampleable, noise_model::AbstractNoiseModel, nsamples) end
 
+"""
+    predictdistr(::EstimationMethod, f, xs, ys, paramprior, noise_model)
+
+Solve the parameter estimation problem ``y = f(x, θ) + ε`` and directly return the distribution of θ.
+
+Depending on the method, the distribution is either generated directly, or constructed by fitting a multivariate normal distribution to generated samples.
+The `paramprior` is used either as a prior for bayesian methods, or to sample initial guesses for iterative methods.
+"""
 function predictdistr(::EstimationMethod, f, xs, ys, paramprior::Sampleable, noise_model::AbstractNoiseModel) end
 
 include("mcmcestimator.jl")
