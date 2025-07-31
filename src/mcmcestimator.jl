12955adcd42af3fd8301f1f8347a873337e65db0
Romeo Valentin <romeov@stanford.edu>
2025-07-30 20:31:17 -0700

    Move Turing stuff to package extension

--- src/mcmcestimator.jl
+++ src/mcmcestimator.jl
@@ -10,50 +10,14 @@ See also [`predictdist`](@ref) and [`predictsamples`](@ref).
 # Fields
 $(TYPEDFIELDS)
 """
-@kwdef struct MCMCEstimator{ST <: Turing.InferenceAlgorithm, SAT <: NamedTuple} <:
+@kwdef struct MCMCEstimator{ST, SAT <: NamedTuple} <:
               EstimationMethod
     "Inference algorithm type for MCMC sampling. Defaults to `NUTS`."
-    samplealg::ST = NUTS()
+    samplealg::ST = nothing
 
     """
-    kwargs passed to `Turing.sample`. 
+    kwargs passed to `Turing.sample`.
     Defaults to `(; drop_warmup=true, progress=false, verbose=false)`.
     """
     sampleargs::SAT = (; drop_warmup = true, progress = false, verbose = false)
-end
-solvealg(est::MCMCEstimator) = est.samplealg
-solveargs(est::MCMCEstimator) = est.sampleargs
-
-@model function bayesianmodel(estimator::MCMCEstimator, f, xs, ysmeas,
-        paramprior::Sampleable, noisemodel::NoiseModel)
-    θ ~ paramprior
-    ys = maybeflatten(f.(xs, [θ]))
-    ysmeas ~ ShiftedDistribution(mvnoisedistribution(noisemodel), ys)
-    return
-end
-
-function predictsamples(est::MCMCEstimator, f, xs, ysmeas,
-        paramprior::Sampleable, noisemodel::NoiseModel, nsamples::Int)
-    # we used to have this `with_logger` closure, however it's giving JET trouble
-    # correctly inferring the type of `est` somehow.
-    # chain = with_logger(ConsoleLogger(Warn)) do  # ignore "Info" outputs.
-    original_logger = global_logger() # Store the current global logger
-    chain = try
-        global_logger(ConsoleLogger(stderr, Logging.Warn))
-        alg = solvealg(est)
-        sample(bayesianmodel(est, f, xs, maybeflatten(ysmeas), paramprior, noisemodel),
-            alg, nsamples;
-            solveargs(est)...)
-    finally
-        global_logger(original_logger) # Always restore the original logger
-    end
-    d = length(paramprior)
-    θsamples = stack([chain[Symbol("θ[$i]")][:] for i in 1:d]; dims = 1)
-    eachcol(θsamples)
-end
-function predictdist(
-        est::MCMCEstimator, f, xs, ys_meas, paramprior::Sampleable, noisemodel::NoiseModel;
-        nsamples::Int = 100)
-    samples = predictsamples(est, f, xs, ys_meas, paramprior, noisemodel, nsamples)
-    fit(MvNormal, parent(samples))
 end
